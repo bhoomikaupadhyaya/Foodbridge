0899d0735ea878b16270e3b787069035dd44ab95
bhoomikaupadhyaya <bhoomikaupadhyaya@gmail.com>
2026-07-12 23:57:14 +0530

    updated

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,42 +1,18 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: "https://foodbridge-backend-mbik.onrender.com/api",
 });
 
-// Attach JWT token automatically
-API.interceptors.request.use(
-  (config) => {
-    const token = localStorage.getItem("token");
+// Automatically attach JWT token
+API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    if (token) {
-      config.headers.Authorization = `Bearer ${token}`;
-    }
-
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
-
-// Handle expired token
-API.interceptors.response.use(
-  (response) => response,
-  (error) => {
-    if (error.response?.status === 401) {
-      localStorage.removeItem("token");
-      localStorage.removeItem("role");
-      localStorage.removeItem("user");
-
-      if (window.location.pathname !== "/") {
-        window.location.href = "/";
-      }
-    }
-
-    return Promise.reject(error);
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
   }
-);
+
+  return config;
+});
 
 export default API;
